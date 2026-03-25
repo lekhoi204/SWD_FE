e0fb264ca796d3f4b75a58ef53691af477b51200
Phuong20042910 <phuongpdtse182721@fpt.edu.vn>
2026-03-25 10:47:37 +0700

    Delete code

--- Frontend-main/src/context/index.ts
+++
@@ -1,3 +0,0 @@
-export { CartProvider, useCart } from './CartContext';
-export { ThemeProvider, useTheme } from './ThemeContext';
-export { AuthProvider, useAuth } from './AuthContext';
--- Frontend-main/src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { HomePage } from "./HomePage";
-export { ProductListPage } from "./ProductListPage";
-export { ProductDetailPage } from "./ProductDetailPage";
-export { PCBuilderPage } from "./PCBuilderPage";
-export { CartPage } from "./CartPage";
-export { CheckoutPage } from "./CheckoutPage";
-export { ProfilePage } from "./ProfilePage";
-export { MyOrdersPage } from "./MyOrdersPage";
